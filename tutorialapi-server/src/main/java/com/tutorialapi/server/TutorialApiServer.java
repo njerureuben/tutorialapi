dcbd0bdeade30769966552e9011336f45ee2d91b
njerureuben <njerureuben03@gmail.com>
2025-09-07 11:53:46 +0300

    Included the Log Back Mechanism in the Server Application

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -2,11 +2,17 @@ package com.tutorialapi.server;
 
 import org.eclipse.jetty.http.HttpScheme;
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TutorialApiServer {
 
+    private  static  final Logger LOGGER = LoggerFactory.getLogger("Server");
+
     public static void main(String[] args) {
 
+        // Test Logger Here
+        LOGGER.error("Hello World");
         HttpConfiguration httpConfiguration = new  HttpConfiguration();
         httpConfiguration.setSecureScheme(HttpScheme.HTTPS.asString());// Turns Http Connector into  a Secure Connector
     }
